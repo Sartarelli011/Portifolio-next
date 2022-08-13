28cde00869c2f08a23f9049ef4d88366816b59b7
Gabriel Sartarelli <gabrielsartarelli@gmail.com>
2022-08-12 21:06:40 -0300

    correções

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   },
   images: {
     loader: "akamai",
-    path: "",
+    disableStaticImages: true,
   },
   basePath: "/Portifolio-next",
   assetPrefix: "/Portifolio-next",
